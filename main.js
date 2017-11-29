889a860ed9efc732bbfd3efdf56619dc5502010b
TvanVliet <dev.timvanvliet@gmail.com>
2017-11-29 13:55:37 +0100

    setup + goals

---
+++ main.js
@@ -0,0 +1,17 @@
+/* The app is supposed to present a suggestion for what to eat.
+functions:
+	- Go through a list with dishes and present one randomly.
+	- Provide recipe or a link to google search recipes.
+	- Try again button
+
+	- Later turn this small project into an android app.
+
+*/
+
+
+
+// get random dish
+function getDish() {
+	// body...
+}
+
